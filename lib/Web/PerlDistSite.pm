ab2961cc89deec4d215f9fdcfe28823cb8cb616a
Toby Inkster <mail@tobyinkster.co.uk>
2023-03-29 22:11:48 +0100

    fix this var

--- lib/Web/PerlDistSite.pm
+++ lib/Web/PerlDistSite.pm
@@ -289,7 +289,7 @@ __DATA__
 	<body>
 		<header></header>
 		<main></main>
-		<div style="height: 150px; overflow: hidden;"><svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;"><path d="M0.00,49.98 C138.82,121.67 349.20,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z" style="stroke: none; fill: rgba(var(--bs-dark-rgb), var(--bs-bg-opacity));"></path></svg></div>
+		<div style="height: 150px; overflow: hidden;"><svg viewBox="0 0 500 150" preserveAspectRatio="none" style="height: 100%; width: 100%;"><path d="M0.00,49.98 C138.82,121.67 349.20,-49.98 500.00,49.98 L500.00,150.00 L0.00,150.00 Z" style="stroke: none; fill: rgba(var(--bs-dark-rgb), 1);"></path></svg></div>
 		<footer></footer>
 		<script src="{{ $root }}assets/scripts/bootstrap.bundle.min.js"></script>
 		<script src="//kit.fontawesome.com/6d700b1a29.js" crossorigin="anonymous"></script>
